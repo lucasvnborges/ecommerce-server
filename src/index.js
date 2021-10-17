bb2753198eeedf781923dc465887a19f6a85610f
lucasnsborges <lucasnsborges@gmail.com>
2021-10-17 20:16:48 -0300

    logger config

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const config = require('./config/config');
 let server;
 
 server = app.listen(config.port, () => {
-    console.log(`Listening to port ${config.port}`);
+  console.log(`Listening to port ${config.port}`);
 });
