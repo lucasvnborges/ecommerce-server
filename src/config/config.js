c0869194057e020dfa3121ffb8e18b95a0edd144
lucasnsborges <lucasnsborges@gmail.com>
2021-10-17 20:21:27 -0300

    lipstick

--- src/config/config.js
+++ src/config/config.js
@@ -5,19 +5,17 @@ const Joi = require('joi');
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const envVarsSchema = Joi.object()
-    .keys({
-        NODE_ENV: Joi.string().valid('production', 'development').required(),
-        PORT: Joi.number().default(3001),
-    })
-    .unknown();
+  .keys({
+    NODE_ENV: Joi.string().valid('production', 'development').required(),
+    PORT: Joi.number().default(3001),
+  })
+  .unknown();
 
-const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key'} })
-    .validate(process.env);
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
-if (error)
-    throw new Error(`Config validation error: ${error.message}`);
+if (error) throw new Error(`Config validation error: ${error.message}`);
 
 module.exports = {
-    env: envVars.NODE_ENV,
-    port: envVars.PORT,
-}
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+};
