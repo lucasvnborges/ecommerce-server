bbc50b34c0c2eb4387248a0b26768fcf99870a7b
lucasnsborges <lucasnsborges@gmail.com>
2021-10-18 09:36:04 -0300

    mongoose config

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,7 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development').required(),
     PORT: Joi.number().default(3001),
+    MONGODB_URL: Joi.string().required().description('MongoDB url'),
   })
   .unknown();
 
@@ -18,4 +19,12 @@ if (error) throw new Error(`Config validation error: ${error.message}`);
 module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
+  mongoose: {
+    url: envVars.MONGODB_URL,
+    options: {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
+const logger = reuiqre('./config/config');
 
 let server;
 
-server = app.listen(config.port, () => {
-  console.log(`Listening to port ${config.port}`);
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info('MongoDB connection on');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
+  });
 });
