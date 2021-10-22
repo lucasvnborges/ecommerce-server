39c90ea107d572e85ab335358d7b0aae320f07e8
lucasnsborges <lucasnsborges@gmail.com>
2021-10-22 17:11:18 -0300

    fix: rename docs file

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const docsRoutes = require('./docs.routes');
+const config = require('../../config/config');
 
 const router = express.Router();
+
+const devRoutes = [
+  {
+    path: '/docs',
+    route: docsRoutes,
+  },
+];
+
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
 
 module.exports = router;
