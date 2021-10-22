7302c5ccc5739c925cf8203f4dc5362bd9f69eda
lucasnsborges <lucasnsborges@gmail.com>
2021-10-22 17:08:20 -0300

    fix: unexpectedErrorHandler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
-const logger = reuiqre('./config/config');
+const logger = require('./config/config');
 
 let server;
 
@@ -10,4 +10,30 @@ mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
   });
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
 });
