8627866d752cf685858ede671a8364b3e874bc9e
lucasnsborges <lucasnsborges@gmail.com>
2021-10-26 19:26:31 -0300

    feat: user register validation

---
+++ src/validations/auth.validation.js
@@ -0,0 +1,15 @@
+const Joi = require('joi')
+const { password } = require('./custom.validation')
+
+const register = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().custom(password),
+    name: Joi.string().required(),
+    role: Joi.string()
+  })
+}
+
+module.exports = {
+  register
+}
